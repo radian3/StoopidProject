5268663f3f093c8b61d04f0acc215823eaf26d84
radian3 <andrewmcneil1337@gmail.com>
2017-12-14 10:23:53 -0600

    SP-2 Set up project on GitHub

---
+++ src/main/java/george/lost/to/a/bot/StoopidProject/MathUtils.java
@@ -0,0 +1,20 @@
+package george.lost.to.a.bot.StoopidProject;
+
+/**
+ * Utility class for basic math operations.
+ * @author radian3
+ */
+public class MathUtils {
+
+
+  /**
+   * Adds two numbers together.
+   * 
+   * @param num1 the first number to add
+   * @param num2 the second number to add
+   * @return the summation of <code>num1</code> and <code>num2</code>
+   */
+  public static int addNums(int num1, int num2) {
+    return num1 + num2;
+  }
+}
